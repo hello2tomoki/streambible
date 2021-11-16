28657b0c58e5bded964e11e3e2bed6f3020d1ba7
hello2tomoki <hello2tomoki@hotmail.com>
2021-11-15 19:13:24 -0800

    updated subprocess

--- nltk_utils.py
+++ nltk_utils.py
@@ -3,3 +3,16 @@ import subprocess
 cmd = ['python3','-m','textblob.download_corpora']
 subprocess.run(cmd)
 print("Working - textblob.download_corpora")
+
+
+### python -c "import nltk; nltk.download('punkt')" 
+cmd2 = ['python','-c',"import nltk; nltk.download('punkt')"]
+subprocess.run(cmd2)
+print("Working - nltk")
+
+
+### python -m textblob.download_corpora;2D
+cmd3 = ['python','-m','textblob.download_corpora']
+subprocess.run(cmd3)
+print("Working - textblob")
+
